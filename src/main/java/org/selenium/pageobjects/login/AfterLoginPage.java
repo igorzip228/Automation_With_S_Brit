ec6eaf0f6675da9ef5803cdd7ee4121d4a6350ca
igorzip228 <igorzip228@gmail.com>
2020-09-16 00:05:18 +0300

    First commit from home

--- src/main/java/org/selenium/pageobjects/login/AfterLoginPage.java
+++ src/main/java/org/selenium/pageobjects/login/AfterLoginPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.selenium.upload_download_page.BasePage;
 import org.selenium.webDriver.DriverHolder;
-import sun.jvm.hotspot.debugger.Page;
+
 
 public class AfterLoginPage extends BasePage {
 //    private WebDriver driver;
